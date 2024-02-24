08661d9c3a91666d8fb4ac0072f8d5755b52b26f
supercici24 <1844680504@qq.com>
2024-02-24 11:17:30 +0800

    给用户信息添加类型

--- src/service/login/login.ts
+++ src/service/login/login.ts
@@ -1,15 +1,26 @@
+import { AxiosHeaders } from 'axios'
 import aniRequest from '..'
 import type { IAccount } from '@/types'
 
 export function accountLoginRequest(account: IAccount) {
   return aniRequest.post({
     url: '/login',
-    data: account
+    data: account,
+    headers: new AxiosHeaders()
   })
 }
 
 export function getUserInfoById(id: number) {
   return aniRequest.get({
-    url: `/users/${id}`
+    url: `/users/${id}`,
+    headers: new AxiosHeaders()
+  })
+}
+
+// 获取用户权限
+export function getUserMenusByRoleId(id: number) {
+  return aniRequest.get({
+    headers: new AxiosHeaders(),
+    url: `/role/${id}/menu`
   })
 }
--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,13 +1,21 @@
 import { defineStore } from 'pinia'
-import { accountLoginRequest, getUserInfoById } from '@/service/login/login'
+import { accountLoginRequest, getUserInfoById, getUserMenusByRoleId } from '@/service/login/login'
 import type { IAccount } from '@/types'
 import { localCache } from '@/utils/cache'
 import router from '@/router'
 import { LOGIN_TOKEN } from '@/global/constants'
 
+interface ILoginState {
+  token: string
+  userInfo: any
+  userMenus: any[]
+}
+
 const useLoginStore = defineStore('login', {
-  state: () => ({
-    token: localCache.getCache(LOGIN_TOKEN) ?? '' //空值合并运算符 ??，判断左侧表达式的值是否为 null 或 undefined，如果是，则返回右侧表达式的值。否则返回左侧表达式的值
+  state: (): ILoginState => ({
+    token: localCache.getCache(LOGIN_TOKEN) ?? '', //空值合并运算符 ??，判断左侧表达式的值是否为 null 或 undefined，如果是，则返回右侧表达式的值。否则返回左侧表达式的值
+    userInfo: {},
+    userMenus: []
   }),
   actions: {
     async loginAccountAction(account: IAccount) {
@@ -15,14 +23,19 @@ const useLoginStore = defineStore('login', {
       const loginResult: any = await accountLoginRequest(account)
       const id = loginResult.data.id
       this.token = loginResult.data.token
-      const name = loginResult.data.name
 
       // 进行本地缓存
       localCache.setCache(LOGIN_TOKEN, this.token)
 
       // 获取登录用户的详细信息（角色信息）
       const userInfoResult = await getUserInfoById(id)
-      console.log('user:', userInfoResult)
+      const userInfo = userInfoResult.data
+      this.userInfo = userInfo
+
+      // 根据角色请求用户的权限（菜单menus）
+      // const userMenusResult = await getUserMenusByRoleId(this.userInfo.role.id)
+      // this.userMenus = userMenusResult.data
+
       // 页面跳转到主页面
       router.push('/main')
     }
